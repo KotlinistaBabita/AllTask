95991e26fe8d1c964a3aa43e8961c28a2a2d9f5c
KotlinistaBabita <babita.bhatt@binarysemantics.com>
2025-04-12 18:39:27 +0530

    ProgressBar

--- app/src/main/java/com/example/pdfviewer/LoginActivity.kt
+++ app/src/main/java/com/example/pdfviewer/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.pdfviewer
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -29,6 +31,8 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     private val RC_SIGN_IN = 100
     private lateinit var userDao: UserDao
+    private lateinit var progressBar: ProgressBar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,15 +41,8 @@ class LoginActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         userDao = UserDatabase.getDatabase(this).userDao()
+        progressBar = findViewById(R.id.progressBar)
 
-
-        lifecycleScope.launch {
-            val user = userDao.getUserByUid(firebaseAuth.currentUser?.uid ?: "")
-            if (user != null) {
-                uploadTokenAndRedirect(user.uid)
-                return@launch
-            }
-        }
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.client_Id))
             .requestEmail()
@@ -61,6 +58,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun signInWithGoogle() {
+        progressBar.visibility = View.VISIBLE
+
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
@@ -76,9 +75,13 @@ class LoginActivity : AppCompatActivity() {
                 if (idToken != null) {
                     authenticateWithFirebase(idToken, account)
                 } else {
+                    progressBar.visibility = View.GONE
+
                     Toast.makeText(this, "Failed to get ID token", Toast.LENGTH_SHORT).show()
                 }
             } else {
+                progressBar.visibility = View.GONE
+
                 Log.e("LoginActivity", "Google sign-in failed", task.exception)
                 Toast.makeText(this, "Google Sign-In Failed", Toast.LENGTH_SHORT).show()
             }
@@ -95,8 +98,11 @@ class LoginActivity : AppCompatActivity() {
 
                     lifecycleScope.launch {
                         saveUserToRoom(account, userId!!)
+                        uploadTokenAndRedirect(userId)
                     }
                 } else {
+                    progressBar.visibility = View.GONE
+
                     Toast.makeText(this, "Firebase Authentication Failed", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -144,6 +150,8 @@ class LoginActivity : AppCompatActivity() {
                         }
                 }
             }
+            progressBar.visibility = View.GONE
+
             startActivity(Intent(this, MainActivity::class.java))
             finish()
 
